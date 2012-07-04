6c467007292c5bacf946ae382ae504ddd06da325
Dmitriy Kiriyenko <dmitriy.kiriyenko@anahoret.com>
2012-07-04 14:51:36 +0300

    Refactor capistrano hook.

--- lib/releaser/capistrano.rb
+++ lib/releaser/capistrano.rb
@@ -1,9 +1,27 @@
-unless Capistrano::Configuration.respond_to?(:instance)
-  abort "Soprano requires Capistrano 2"
-end
-
 Capistrano::Configuration.instance(:must_exist).load do
-  Dir["#{File.dirname(__FILE__)}/capistrano/*.rb"].each do |recipe|
-    load(recipe)
+  namespace :releaser do
+    desc "Write current revision to CURRENT_VERSION file."
+    task :write_current do
+      version = self[:current_revision]
+      run "echo \"#{version}\" > #{release_path}/CURRENT_VERSION"
+    end
+
+    task :tag_deploy_commit do
+      run_locally "bundle exec releaser deploy --push --object=#{real_revision}"
+    end
+  end
+
+  on :load do
+    set :current_revision do
+      run_locally("bundle exec releaser info --no-verbose").chomp
+    end
+
+    if fetch(:write_version, true)
+      after "deploy:create_symlink", "releaser:write_current"
+    end
+
+    if fetch(:tag_deploy_commit, true)
+      before "deploy:update_code", "releaser:tag_deploy_commit"
+    end
   end
 end
